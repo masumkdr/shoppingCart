425856e3dabca5aac8b5a58256737830c8ee830b
Masum Khandker <masum@LAPTOP.turnover.com>
2020-10-14 08:58:54 +0600

    total amount updated

--- js/script.js
+++ js/script.js
@@ -18,7 +18,9 @@ packages.addEventListener('click', (e) => {
     if(e.target.hasAttribute('href')) {
       let item = e.target.parentElement.parentElement;
       showCarts(item);
+      showAmount(item);
     }
+    e.preventDefault();
 });
 
 // remove items from cart
@@ -26,7 +28,9 @@ cart.addEventListener("click", (item) => {
      if(item.target.hasAttribute('href')) {
          let rmItem = item.target.parentElement.parentElement.parentElement;
          rmItem.remove();
+         minusAmount(rmItem);
      }
+     item.preventDefault();
 })
 
 function showPackages(package) {
@@ -41,9 +45,9 @@ function addToPackages(pack) {
     card.className = 'col-md-4';
     card.innerHTML = `
                     <div class="card">
-                    <div class="card-header card-title bg-info text-light py-3 mb-3">
+                    <div class="card-header card-title bg-info text-light text-uppercase py-3 mb-3">
                         <h4 id="itemname">${pack.name}</h4></div>
-                    <img class="card-img-top" src="${pack.image}" alt="Card image cap" height="300" width="100">
+                    <img class="card-img-top" src="${pack.image}" alt="Card image cap" height="150" width="30">
                     <div class="card-body">
                             <ul>
                                 <li>Size : ${pack.size}</li>
@@ -73,16 +77,34 @@ function showCarts(item) {
         <div class="col-md-3">
             <a href="#"  id="delete" class="delete text-danger">X</a>
         </div>
-        <div class="col-md-3 d-flex align-items-left">
+        <div class="col-md-3 text-uppercase">
             <p>${itemName}</p>
         </div>
-        <div class="col-md-3 d-flex align-items-right">
+        <div class="col-md-3">
             <p>${quantity}</p>
         </div>
-        <div class="col-md-3vd-flex align-items-right">
+        <div class="col-md-3">
             <p>${price}</p>
         </div>
     </div>
     `;
     cart.appendChild(li);
+    
+}
+
+function showAmount(e) {
+    let total = document.querySelector("#total");
+    amount = parseInt(total.getElementsByTagName("span")[1].innerHTML);
+    let itemPrice = e.getElementsByTagName("H1")[0].innerHTML;
+    itemPrice = parseInt(itemPrice.split(" ")[1]);
+    total.getElementsByTagName("span")[1].innerHTML = amount + itemPrice;
+
+}
+
+function minusAmount(minItem) {
+    let total = document.querySelector("#total");
+    amount = parseInt(total.getElementsByTagName("span")[1].innerHTML);
+    let itemAmount = minItem.getElementsByTagName("p")[2].innerHTML;
+    itemAmount = parseInt(itemAmount.split(" ")[1]);
+    total.getElementsByTagName("span")[1].innerHTML = amount - itemAmount;
 }
